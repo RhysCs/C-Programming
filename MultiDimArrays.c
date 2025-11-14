436734a14b8233b38724821b2f49eb388895ee79
RhysCs <rhyscarrsmith@gmail.com>
2025-11-14 16:29:08 +1300

    Multi arrays

--- MultiDimArrays.c
+++ MultiDimArrays.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 void main(void){
+    /* Define the multi array, not that it is not required to define the number 
+    of rows but it must know how many integers are in the array*/
 int a[2][5] ={ 
             {2, 3, 4, 6, 3},
             {1, 2, 6, 2, 6}
             };
 
-int b;
-int c;
+// Variables 
 int i;
+int j;
 
-while (i < 10){
-    
-    for(b = 0; b < 2; b++){
-        for(c = 0; c < 5; c++){
-            printf("%d, %d", a[i][i]);
+int row = 5;
+int col = 2;
+
+// For loop to loop around the rows
+for(i = 0; col > i; i++){
+    // loop around the rows and then print
+    /* Note that first it loops around the col increments i 
+    then loops and around the rows and increments j then it 
+    repeats again with the next increment*/
+    for(j = 0; row > j; j++){
+        printf("%d, ", a[i][j]);
 
         }
+        printf("\n");
     }
     i++;
 }
 
 
-}
